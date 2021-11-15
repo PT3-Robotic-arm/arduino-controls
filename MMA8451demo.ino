d24384532787fd114c850dca43467ff352d746ba
Zalaxx <leo.jaillet@protonmail.com>
2021-11-15 21:55:44 +0100

    update code

--- MMA8451demo.ino
+++ MMA8451demo.ino
@@ -28,7 +28,7 @@ Adafruit_MMA8451 mma = Adafruit_MMA8451();
 void setup(void) {
   Serial.begin(9600);
   
-  Serial.println("Adafruit MMA8451 test!");
+  //Serial.println("Adafruit MMA8451 test!");
   
 
   if (! mma.begin()) {
@@ -36,62 +36,63 @@ void setup(void) {
    while (1);
   }
   
-  Serial.println("MMA8451 found!");
+  //Serial.println("MMA8451 found!");
   
   mma.setRange(MMA8451_RANGE_2_G);
   
-  Serial.print("Range = "); Serial.print(2 << mma.getRange());  
-  Serial.println("G");
+  //Serial.print("Range = "); Serial.print(2 << mma.getRange());  
+  //Serial.println("G");
   
 }
 
 void loop() {
   // Read the 'raw' data in 14-bit counts
   mma.read();
-  Serial.print("X:\t"); Serial.print(mma.x); 
-  Serial.print("\tY:\t"); Serial.print(mma.y); 
-  Serial.print("\tZ:\t"); Serial.print(mma.z); 
-  Serial.println();
+  Serial.print("{\"x\": \""); Serial.print(mma.x); 
+  Serial.print("\", \"y\": \""); Serial.print(mma.y); 
+  Serial.print("\", \"z\": \""); Serial.print(mma.z);
+  
 
   /* Get a new sensor event */ 
   sensors_event_t event; 
   mma.getEvent(&event);
 
   /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: \t"); Serial.print(event.acceleration.x); Serial.print("\t");
-  Serial.print("Y: \t"); Serial.print(event.acceleration.y); Serial.print("\t");
-  Serial.print("Z: \t"); Serial.print(event.acceleration.z); Serial.print("\t");
-  Serial.println("m/s^2 ");
+  Serial.print("\", \"acc_x\": \""); Serial.print(event.acceleration.x);
+  Serial.print("\", \"acc_y\": \""); Serial.print(event.acceleration.y);
+  Serial.print("\", \"acc_z\": \""); Serial.print(event.acceleration.z); 
   
   /* Get the orientation of the sensor */
   uint8_t o = mma.getOrientation();
   
+  Serial.print("\", \"direction\": \"");
   switch (o) {
     case MMA8451_PL_PUF: 
-      Serial.println("Portrait Up Front");
+      Serial.print("Portrait Up Front");
       break;
     case MMA8451_PL_PUB: 
-      Serial.println("Portrait Up Back");
+      Serial.print("Portrait Up Back");
       break;    
     case MMA8451_PL_PDF: 
-      Serial.println("Portrait Down Front");
+      Serial.print("Portrait Down Front");
       break;
     case MMA8451_PL_PDB: 
-      Serial.println("Portrait Down Back");
+      Serial.print("Portrait Down Back");
       break;
     case MMA8451_PL_LRF: 
-      Serial.println("Landscape Right Front");
+      Serial.print("Landscape Right Front");
       break;
     case MMA8451_PL_LRB: 
-      Serial.println("Landscape Right Back");
+      Serial.print("Landscape Right Back");
       break;
     case MMA8451_PL_LLF: 
-      Serial.println("Landscape Left Front");
+      Serial.print("Landscape Left Front");
       break;
     case MMA8451_PL_LLB: 
-      Serial.println("Landscape Left Back");
+      Serial.print("Landscape Left Back");
       break;
     }
+  Serial.print("\"};");
   Serial.println();
   delay(500);
   
